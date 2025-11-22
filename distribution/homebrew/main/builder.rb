952466ae0aeb1a4ba9ecc35a658a4fed85c682d7
griffin <gjstrier@gmail.com>
2025-11-21 17:54:48 -0800

    Update Homebrew formula to v1.0.6 with SHA256

--- distribution/homebrew/main/builder.rb
+++ distribution/homebrew/main/builder.rb
@@ -2,7 +2,7 @@ class Builder < Formula
   desc "High-performance build system for mixed-language monorepos"
   homepage "https://github.com/GriffinCanCode/Builder"
   url "https://github.com/GriffinCanCode/Builder/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "00be5a007a9b6b33c572e544426598cdf4c63c7dca875c9bdd2e1d6d7edfb7ad"
   license "MIT"
   head "https://github.com/GriffinCanCode/Builder.git", branch: "master"
 
